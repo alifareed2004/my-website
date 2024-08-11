94d6ac0a60798bf7937bcd226d2ad797178c911a
Ali <alifareed2004@gmail.com>
2024-08-11 18:28:29 -0400

    Finished Dark Mode Implementation

--- DarkMode.js
+++ DarkMode.js
@@ -1,4 +1,34 @@
-function darkModeFunction() {
+/* Initialize variables */
+let current_mode = localStorage.getItem("current_mode");
+const dark_mode_button = document.getElementById("dark_mode_button");
+
+/* Enable Dark Mode Function */
+const enableDarkMode = () => {
+    document.body.classList.add("dark_mode");
+    localStorage.setItem("current_mode", "active");
+}
+
+/* Disable Dark Mode Function */
+const disableDarkMode = () => {
+    document.body.classList.remove("dark_mode")
+    localStorage.setItem("current_mode", null);
+}
+
+/* If dark mode is already active, enable it */
+if (current_mode === "active") enableDarkMode();
+
+/* Checks if the dark mode button is pressed and executes the appropriate function */
+dark_mode_button.addEventListener("click", () => {
+    current_mode = localStorage.getItem("current_mode");
+    if (current_mode !== "active") {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+})
+
+/* OLD DARK MODE FUNCTION */
+/*function darkModeFunction() {
     var element = document.body;
     element.classList.toggle("dark_mode");
- }
+ }*/
