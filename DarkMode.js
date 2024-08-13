8497a6ff54531697d9922f42d9a1a920e5cfdf7d
Ali <alifareed2004@gmail.com>
2024-08-12 21:45:10 -0400

    Finished blog posts and improved dark mode

--- DarkMode.js
+++ DarkMode.js
@@ -1,23 +1,25 @@
-/* Initialize variables */
+// Initialize variables
 let current_mode = localStorage.getItem("current_mode");
 const dark_mode_button = document.getElementById("dark_mode_button");
 
-/* Enable Dark Mode Function */
+// Enable Dark Mode Function
 const enableDarkMode = () => {
     document.body.classList.add("dark_mode");
     localStorage.setItem("current_mode", "active");
+    dark_mode_button.innerHTML = "Light Mode";
 }
 
-/* Disable Dark Mode Function */
+// Disable Dark Mode Function
 const disableDarkMode = () => {
     document.body.classList.remove("dark_mode")
     localStorage.setItem("current_mode", null);
+    dark_mode_button.innerHTML = "Dark Mode";
 }
 
-/* If dark mode is already active, enable it */
+// If dark mode is already active, enable it
 if (current_mode === "active") enableDarkMode();
 
-/* Checks if the dark mode button is pressed and executes the appropriate function */
+// Checks if the dark mode button is pressed and executes the appropriate function
 dark_mode_button.addEventListener("click", () => {
     current_mode = localStorage.getItem("current_mode");
     if (current_mode !== "active") {
@@ -26,9 +28,3 @@ dark_mode_button.addEventListener("click", () => {
         disableDarkMode();
     }
 })
-
-/* OLD DARK MODE FUNCTION */
-/*function darkModeFunction() {
-    var element = document.body;
-    element.classList.toggle("dark_mode");
- }*/
